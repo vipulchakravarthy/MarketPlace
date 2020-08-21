9a6e9964a02dd22a50e3c4bb2259f3e2ae49bc12
vipul chakravarthy <41899503+vipulchakravarthy@users.noreply.github.com>
2020-08-22 02:54:48 +0530

    register screen

--- App.js
+++ App.js
@@ -30,8 +30,9 @@ import AppTextInput from "./app/components/AppTextInput";
 import AppPicker from "./app/components/AppPicker"
 import LoginScreen from "./app/screens/LoginScreen"
 import ListingEditScreen from "./app/screens/ListingEditScreen";
+import RegisterScreen from "./app/screens/RegisterScreen";
 
 export default function App() {
 
-  return <ListingEditScreen />;
+  return <RegisterScreen />;
 }
